84d0bd9a693c9c305300b1ee49e7bc4fe09532ad
Rafael Toledo Nunes <rafael.nunes1724@gmail.com>
2025-07-15 17:56:27 -0300

    Led and button.c

--- Led and button.c
+++ Led and button.c
@@ -1,29 +1,25 @@
 #include <Arduino.h>
-#define pinLed 15
-#define pinButton 22
 
-int count = 0;
+const int LED = 13;
+const int Button = 10;
+bool stateButton;
 
 void setup()
 {
-  pinMode(pinLed, OUTPUT);
-  pinMode(pinButton, INPUT_PULLUP);
+  pinMode(Button,INPUT);
+  pinMode(LED,OUTPUT);
   Serial.begin(9600);
+  
+  
 }
 
 void loop()
 {
+ stateButton = digitalRead(Button);
+ Serial.println(stateButton);
+ digitalWrite(LED,stateButton);
 
-  bool CurrentStatebutton = analogRead(pinButton);
-  static bool stateLed;
-  static bool previousStatebutton;
-  if (CurrentStatebutton == 1 && previousStatebutton == 0)
-  {
-
-    stateLed = !stateLed;
-    digitalWrite(pinLed, stateLed);
-    count++;
-    Serial.println(count);
-  }
-  previousStatebutton = CurrentStatebutton;
+  
+  
+  
 }
